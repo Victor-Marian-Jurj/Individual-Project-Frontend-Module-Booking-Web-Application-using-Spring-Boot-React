5d61622caf6ff8461130b253574c9800629058b6
victorjurj <victor.jurj@yahoo.com>
2023-11-02 15:28:42 +0200

    add ViewHotel Cancel button and its functionality

--- src/pages/hotel/ViewHotel.jsx
+++ src/pages/hotel/ViewHotel.jsx
@@ -1,14 +1,30 @@
-import { useParams } from "react-router-dom";
+// import { useParams } from "react-router-dom";
 import { useHotelById } from "../../hooks/useHotelById";
-import { CircularProgress } from "@mui/material";
+// import { CircularProgress } from "@mui/material";
 import HotelForm from "./HotelForm";
+import { Button, CircularProgress } from "@mui/material";
+import { useNavigate, useParams } from "react-router-dom";
 
 const ViewHotel = () => {
   const { hotelId } = useParams();
   const { hotel } = useHotelById(hotelId);
+  const navigate = useNavigate();
+
+  const handleCancelClick = () => {
+    navigate("/hotels");
+  };
 
   return hotel ? (
-    <HotelForm formTitle="View hotel" hotel={hotel} isReadonly={true} />
+    <div>
+      <HotelForm formTitle="View hotel" hotel={hotel} isReadonly={true} />
+      <Button
+        variant="outlined"
+        onClick={handleCancelClick}
+        sx={{ mt: "16px" }}
+      >
+        Cancel
+      </Button>
+    </div>
   ) : (
     <CircularProgress />
   );
