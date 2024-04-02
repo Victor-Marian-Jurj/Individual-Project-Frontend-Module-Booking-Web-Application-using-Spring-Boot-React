5912856ebe1c20758262e8bb812d115fef4e03d8
victorjurj <victor.jurj@yahoo.com>
2024-04-02 15:06:58 +0300

    add EmailStatusDialog component for email status

---
+++ src/pages/reservation/EmailStatusDialog.jsx
@@ -0,0 +1,49 @@
+import React from "react";
+import {
+  Button,
+  Dialog,
+  DialogTitle,
+  DialogContent,
+  DialogActions,
+  Typography,
+} from "@mui/material";
+
+const EmailStatusDialog = ({
+  isOpen,
+  onClose,
+  emailSent,
+  invalidEmail,
+  validEmail,
+}) => {
+  return (
+    <Dialog open={isOpen} onClose={onClose}>
+      <DialogTitle>Email Status</DialogTitle>
+      <DialogContent>
+        {emailSent ? (
+          <Typography sx={{ color: "green", fontWeight: "bold" }}>
+            Email has been sent!
+          </Typography>
+        ) : (
+          invalidEmail && (
+            <Typography sx={{ color: "red", fontWeight: "bold" }}>
+              Incorrect email address!
+            </Typography>
+          )
+        )}
+
+        {validEmail && (
+          <Typography sx={{ color: "green", fontWeight: "bold" }}>
+            Email has been sent!
+          </Typography>
+        )}
+      </DialogContent>
+      <DialogActions>
+        <Button onClick={onClose} variant="contained">
+          Close
+        </Button>
+      </DialogActions>
+    </Dialog>
+  );
+};
+
+export default EmailStatusDialog;
